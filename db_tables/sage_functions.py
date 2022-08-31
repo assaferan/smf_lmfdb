8eecfc12220495fb8c2869f9df02441441c117f4
assaferan <assaferan@gmail.com>
2022-08-31 19:06:11 -0400

    bugfix

--- db_tables/sage_functions.py
+++ db_tables/sage_functions.py
@@ -20,7 +20,8 @@ def Hecke_Eigenvalues_Siegel_Eisenstein(k,j,e,prime_bound=MAX_P+1):
     return Hecke_Eigenvalues_Siegel_Eisenstein_Series_All(k,j,e,prime_bound=prime_bound)
 
 # TODO : this is highly inefficient, can sieve it through, and can compue the exact recurrence relation for powers of p
-def Get_All_Hecke_Eigenvalues_Up_To(prec, ap, ap2, j, k):
+def Get_All_Hecke_Eigenvalues_Up_To(prec, ap, ap2, wt):
+    k,j = wt
     a = []
     primes = prime_range(prec+1)
     prime_idx = {primes[i] : i for i in range(len(primes))}
