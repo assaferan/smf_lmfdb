d0906ebbe46583ee842efdfcaf7b885f24b8e633
Eran Assaf <assaferan@gmail.com>
2023-07-09 21:57:39 -0400

    syncing hecke_nf with newforms

--- db_tables/common_populate.py
+++ db_tables/common_populate.py
@@ -132,10 +132,10 @@ def write_data(table, entries, entry_postprocess, aux_fname, folder):
     for i,e in enumerate(triple_entries):
         space_label = make_space_label(e, False)
         space_num_forms[space_label] = space_num_forms.get(space_label,0) + 1
-        e = entry_postprocess(e, {'id' : i,
-                                  'num_forms' : space_num_forms[space_label]})
         if ('hecke_ring_numerators' in keys) and ('hecke_ring_numerators' not in e):
             continue
+        e = entry_postprocess(e, {'id' : i,
+                                  'num_forms' : space_num_forms[space_label]})
         e = fill_nulls(e, table)
         write_entry(e, folder)
         e_datum = '|'.join([entry_to_text(e[k], table.col_type[k]) for k in keys])
