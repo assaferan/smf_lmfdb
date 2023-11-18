e20bc38b8b1739247c94e76e6691af4086d4d07b
Eran Assaf <assaferan@gmail.com>
2023-11-18 05:48:15 -0500

    handling oldforms

--- db_tables/smf_newspaces_populate.py
+++ db_tables/smf_newspaces_populate.py
@@ -1,4 +1,4 @@
-from sage.all import (is_square, is_squarefree, prime_range, factor)
+from sage.all import (is_square, is_squarefree, prime_range, factor, divisors)
 from smf_lmfdb.db_tables.common_populate import make_space_label, entry_add_common_columns, table_reload, get_hecke, common_entry_values, base_26, write_data_from_files
 from smf_lmfdb.db_tables.common_create_table import SUBSPACE_TYPES, HECKE_TYPES
 from smf_lmfdb.db_tables.sage_functions import smf_dims_degree_2_level_1, smf_dims_degree_2_level_2, Hecke_Eigenvalues_Traces_Siegel_Eisenstein, Hecke_Eigenvalue_Traces_Klingen_Eisenstein, Hecke_Eigenvalue_Traces_Saito_Kurokawa, Hecke_Eigenvalue_Traces_Yoshida, num_forms_Siegel_Eisenstein, num_forms_Klingen_Eisenstein, num_forms_Saito_Kurokawa, num_forms_Yoshida
@@ -88,6 +88,7 @@ def create_entries(triple_list):
                 entry['ALdims_G'] = al_dims_G
                 entry['ALdims_P'] = [0 for al in al_dims_G]
                 entry['ALdims'] = [0 for al in al_dims_G]
+                divs = [d for d in divisors(N) if is_squarefree(d)]
                 for i in range(len(divs)):
                     entry['ALdims_P'][i] = Saito_Kurokawa_lift_dim(k,N,al=divs[i])
                     entry['ALdims'][i] = entry['ALdims_G'][i] + entry['ALdims_P'][i]
